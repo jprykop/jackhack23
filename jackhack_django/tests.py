231459e49241a2bfb9cc9bc6f72e44950c0d07f3
Jonathan Prykop-Dennis <github@prykop.com>
2023-12-29 16:59:12 -0600

    Renamed test

--- jackhack_django/tests.py
+++ jackhack_django/tests.py
@@ -11,6 +11,6 @@ class SaveGameTest(GameTestCase, TestCase):
     reloaded_game = SaveGame.objects.get(id=self.game().id)
     self.assertEqual(reloaded_game.player_name, "Jack")
 
-  def test_started_game_saved(self):
+  def test_saved_game_started(self):
     reloaded_game = SaveGame.objects.get(id=self.started_game().id)
     self.assertEqual(self.days_to_dicts(reloaded_game), GameTestCase.EXPECTED_DAYS)
